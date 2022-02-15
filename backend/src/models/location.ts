6b54b5308ab7f5f9f42e200cbb0d6fce525bd33b
Ananya Bhat <ananyab@andrew.cmu.edu>
2022-02-15 10:01:26 -0500

    Fixed syntax errors in location model

--- backend/src/models/location.ts
+++ backend/src/models/location.ts
@@ -8,10 +8,10 @@ export interface LocationInterface extends Document {
 }
 
 export const LocationSchema = new Schema({
-    street_address: {type: String, required: true},,
-    city: {type: String, required: true},,
-    state: {type: String, required: true},,
-    zipcode: {type: String, required: true},
+    street_address: {type: String, required: true},
+    city: {type: String, required: true},
+    state: {type: String, required: true},
+    zipcode: {type: String, required: true}
 });
 
 export const LocationModel: Model<LocationInterface> = mongoose.model('Location', LocationSchema);
