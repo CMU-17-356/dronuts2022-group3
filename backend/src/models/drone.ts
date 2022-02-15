63362a5606af847b08a400c8c6cc5a7706d9d64f
Ananya Bhat <ananyab@andrew.cmu.edu>
2022-02-15 10:55:10 -0500

    Fixed drone model to use location and order schemas instead of models

--- backend/src/models/drone.ts
+++ backend/src/models/drone.ts
@@ -1,20 +1,20 @@
 
 import mongoose, { Schema, Document, Model } from 'mongoose';
-import { LocationInterface, LocationModel } from './location';
-import { OrderInterface, OrderModel } from './order';
+import { LocationInterface, LocationSchema } from './location';
+import { OrderInterface, OrderSchema } from './order';
 
 
 export interface DroneInterface extends Document {
     battery_life: number,
     drone_location: LocationInterface,
-    drone_orderId: OrderInterface,
+    drone_order: OrderInterface,
     critical: boolean
 }
 
 export const DroneSchema = new Schema({
     battery_life: {type: Number, required: true},
-    drone_location: {type: LocationModel, required: true},
-    drone_orderId: {type: OrderModel, required: true},
+    drone_location: {type: LocationSchema, required: true},
+    drone_order: {type: OrderSchema, required: true},
     critical: {type: Boolean, required: true},
 });
 
