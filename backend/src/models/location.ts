2a16ec890500d6fa98918e688d362c7061ede867
Ananya Bhat <ananyab@andrew.cmu.edu>
2022-02-15 00:52:43 -0500

    Introduced location, donut, and order model, as well as dummy drone model

--- backend/src/models/location.ts
+++ backend/src/models/location.ts
@@ -1,9 +1,17 @@
 import mongoose, { Schema, Document, Model } from 'mongoose';
 
-// TODO (ananya) : create the actual model/interface
+export interface LocationInterface extends Document {
+    street_address: string,
+    city: string,
+    state: string,
+    zipcode: string
+}
 
-export interface LocationInterface extends Document {}
-
-export const LocationSchema = new Schema({});
+export const LocationSchema = new Schema({
+    street_address: {type: String, required: true},,
+    city: {type: String, required: true},,
+    state: {type: String, required: true},,
+    zipcode: {type: String, required: true},
+});
 
 export const LocationModel: Model<LocationInterface> = mongoose.model('Location', LocationSchema);
