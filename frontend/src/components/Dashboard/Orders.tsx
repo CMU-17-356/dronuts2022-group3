a5f2062b5bb879a4463d036e244f0f5096463616
santoniriccardo <santoni.riccardo@gmail.com>
2022-02-15 23:12:23 -0500

    Improved aspects of employee page, and got rid of top navbar

--- frontend/src/components/Dashboard/Orders.tsx
+++ frontend/src/components/Dashboard/Orders.tsx
@@ -95,31 +95,6 @@ export default function Orders() {
         </Card>
         </TableRow>
       ))}
-      {/* <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{`$${row.amount}`}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
-        See more orders
-      </Link> */}
       </TableBody>
       </Table>
     </React.Fragment>
