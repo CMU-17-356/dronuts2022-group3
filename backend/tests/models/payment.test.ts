4b63ee4753d86ad178f216031723d92a48848bd8
santoniriccardo <santoni.riccardo@gmail.com>
2022-02-15 21:57:33 -0500

    Fixed failing tests due to payment model/test

--- backend/tests/models/payment.test.ts
+++ backend/tests/models/payment.test.ts
@@ -4,16 +4,14 @@ jest.useFakeTimers()
 describe('payment test', function() {
 
     it('should take on assigned values', () => {
-        const c = new OrderModel();
-        c.payment_id = 57
+        const c = new PaymentModel();
         c.order_total = 31.50
 
-        expect(c.payment).toEqual(57);
         expect(c.order_total).toEqual(31.50);
     });
 
     it('should be invalid if a field is empty', () => {
-        const c = new OrderModel();
+        const c = new PaymentModel();
         
         c.validate(function(err) {
             expect(err).toBeDefined();
