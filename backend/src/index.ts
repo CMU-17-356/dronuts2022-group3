e3dd28e4175cb633e6a13f71cb4d162361565a23
santoniriccardo <santoni.riccardo@gmail.com>
2022-02-22 15:00:51 -0500

    Introduced new connection with mongoose, able to save new documents now

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,19 +3,30 @@ import bodyParser from 'body-parser';
 import Routes from './routes';
 import mongoose from 'mongoose';
 
-const app: Application = express();
-const routes: Routes = new Routes(app);
-const port = 3001;
+var mongoDBUrl = "mongodb://localhost:27017/dronuts"
 
+mongoose.connect(mongoDBUrl);
+
+var db = mongoose.connection;
+
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+const app: Application = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const routes: Routes = new Routes(app);
+const port = 3001;
+
+
 app.get('/', (req, res) => {
   res.status(200).send('Hello Group 3! - Riccardo Santoni');
-});
-
-mongoose.connect('mongodb://localhost:27017/dronuts', {}, () => {
-  console.log('connected to mongodb database')
 });
 
 app.listen(port, () => {
