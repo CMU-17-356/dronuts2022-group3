05d59ee47d4269ed0f9a74bccde6c5db18a5ea97
santoniriccardo <santoni.riccardo@gmail.com>
2022-02-21 23:42:48 -0500

    Introduced basic controller for customer view

---
+++ backend/src/controllers/CustomerController.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express';
+
+export default class CustomersController {
+  constructor() { }
+
+  async getAllCustomers(req: Request, res: Response, next: NextFunction) {
+    res.json("Hah no customers right now. Need to connect to mongoose here and actually return to API user.")
+  }
+}
