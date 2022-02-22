98e1e06c2599f495ad3a00b4ad2d703be5e07966
santoniriccardo <santoni.riccardo@gmail.com>
2022-02-22 13:53:45 -0500

    Introducing mongodb connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import Routes from './routes';
+import mongoose from 'mongoose';
 
 const app: Application = express();
 const routes: Routes = new Routes(app);
@@ -11,6 +12,10 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello Group 3! - Riccardo Santoni');
+});
+
+mongoose.connect('mongodb://localhost:27017/dronuts', {}, () => {
+  console.log('connected to mongodb database')
 });
 
 app.listen(port, () => {
