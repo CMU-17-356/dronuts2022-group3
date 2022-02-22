f17f85dd7066058bed55d4c382ce1f754f1fc399
santoniriccardo <santoni.riccardo@gmail.com>
2022-02-22 13:22:00 -0500

    Introduced more routes/controller models for customer model

--- backend/src/controllers/CustomerController.ts
+++ backend/src/controllers/CustomerController.ts
@@ -1,13 +1,30 @@
 import { Request, Response, NextFunction } from 'express';
+import { apiErrorHandler } from './../handlers/errorHandler';
 
 export default class CustomersController {
   constructor() { }
 
-  async getAllCustomers(req: Request, res: Response, next: NextFunction) {
-    res.json("Hah no customers right now. Need to connect to mongoose here and actually return to API user.");
+  async findAll(req: Request, res: Response, next: NextFunction) {
+    try {
+      res.json("Hah no customers right now. Need to connect to mongoose here and actually return to API user.");
+    } catch (error) {
+      apiErrorHandler(error, req, res, 'Fetch All Courses failed.');
+    }
   }
 
-  async getCustomer(req: Request, res: Response, next: NextFunction) {
+  async findOne(req: Request, res: Response, next: NextFunction) {
     res.json("Return one customer lol. Need to implement.");
+  }
+
+  async create(req: Request, res: Response, next: NextFunction) {
+    res.json("Create one customer lol. Need to implement.");
+  }
+
+  async update(req: Request, res: Response, next: NextFunction) {
+    res.json("Update one customer lol. Need to implement.");
+  }
+
+  async delete(req: Request, res: Response, next: NextFunction) {
+    res.json("Delete one customer lol. Need to implement.");
   }
 }
--- backend/src/routes/CustomerRoutes.ts
+++ backend/src/routes/CustomerRoutes.ts
@@ -9,8 +9,11 @@ class CustomerRoutes {
     this.intializeRoutes();
   }
   intializeRoutes() {
-    this.router.route('/').get(this.customerController.getAllCustomers);
-    this.router.route('/:id').get(this.customerController.getCustomer);
+    this.router.route('/').get(this.customerController.findAll);
+    this.router.route('/').post(this.customerController.create);
+    this.router.route('/:id').get(this.customerController.findOne);
+    this.router.route('/:id').put(this.customerController.update);
+    this.router.route('/:id').delete(this.customerController.delete);
   }
 }
 export default new CustomerRoutes().router;
