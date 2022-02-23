704d3e357eb48ba0d0728fcf0164603af2433532
santoniriccardo <santoni.riccardo@gmail.com>
2022-02-22 19:51:00 -0500

    Introduced routes/controller for store api

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Application } from 'express';
 import customerRouter from './CustomerRoutes';
+import storeRouter from './StoreRoutes';
 
 export default class Routes {
 
   constructor(app: Application) {
     app.use('/api/v1/customers', customerRouter);
+    app.use('/api/v1/stores', storeRouter);
   }
 }
